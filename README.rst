9d6385827e4834d0ce494b4c95f1ba6c21299f27
Bogdan Girman <bogdan.girman@gmail.com>
2014-04-07 01:08:44 +0300

    Update package info

---
+++ README.rst
@@ -0,0 +1,15 @@
+Introduction
+============
+
+.. content::
+
+This product adds `Personal tools` portlet.
+
+
+
+Contributors
+============
+
+- Bogdan Girman, Package Creator
+- Michael Krishtopa, Code Refactoring
+- Denys Mishunov, Code Author
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = open("collective/portlet/personaltools/version.txt").read()
 
 setup(name='collective.portlet.personaltools',
       version=version,
       description="Plone personal tools portlet",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
@@ -14,10 +14,10 @@ setup(name='collective.portlet.personaltools',
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      keywords='portlet personal tools',
+      author='Bogdan Girman',
+      author_email='bogdan.girman@gmail.com',
+      url='https://github.com/collective/collective.portlet.personaltools',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.portlet'],
